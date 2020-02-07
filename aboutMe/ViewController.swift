4e986c6a83b7bb3449ee7fff1bf57bc099391397
Kostya <gdskostya@yandex.ru>
2020-02-07 10:58:33 +0300

    Initial Commit

---
+++ aboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aboutMe
+//
+//  Created by Коstya on 07.02.2020.
+//  Copyright © 2020 Коstya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
