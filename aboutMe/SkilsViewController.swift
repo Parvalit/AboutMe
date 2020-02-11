1bf4adb570bc63c46170eca02439e9e7708363bd
Kostya <gdskostya@yandex.ru>
2020-02-11 15:34:29 +0300

    comit2

---
+++ aboutMe/SkilsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SkilsViewController.swift
+//  aboutMe
+//
+//  Created by Коstya on 11.02.2020.
+//  Copyright © 2020 Коstya. All rights reserved.
+//
+
+import UIKit
+
+class SkilsViewController: UIViewController {
+
+
+    @IBAction func ExitSkils(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
